62d5b2641a349ec283d58b390df738d4e9a1350c
MattFaus <mattfaus@khanacademy.org>
2013-06-28 17:37:37 -0700

    Enable extracting zoneinfo.zip when it is nested inside of third_party.zip (or, really, any number of ZipFiles)
    Test Plan:
    echo hello world > hi.txt
    zip wrap1.zip hi.txt
    zip wrap2.zip wrap1.zip
    zip wrap3.zip wrap2.zip
    
    print extract_nested_zipfile('/Users/mattfaus/dev/dev-git/wrap1.zip').open('hi.txt').read()
    print extract_nested_zipfile('/Users/mattfaus/dev/dev-git/wrap2.zip/wrap1.zip').open('hi.txt').read()
    print extract_nested_zipfile('/Users/mattfaus/dev/dev-git/wrap3.zip/wrap2.zip/wrap1.zip').open('hi.txt').read()

--- pytz/gae.py
+++ pytz/gae.py
@@ -51,11 +51,46 @@ zoneinfo_path = os.path.abspath(os.path.join(os.path.dirname(__file__),
     'zoneinfo.zip'))
 
 
+def extract_nested_zipfile(path, parent_zip=None):
+    """Returns a ZipFile specified by path, even if the path contains
+    intermediary ZipFiles.  For example, /root/gparent.zip/parent.zip/child.zip
+    will return a ZipFile that represents child.zip
+    """
+
+    def extract_inner_zipfile(parent_zip, child_zip_path):
+        """Returns a ZipFile specified by child_zip_path that exists inside
+        parent_zip.
+        """
+        memory_zip = StringIO()
+        memory_zip.write(parent_zip.open(child_zip_path).read())
+        return zipfile.ZipFile(memory_zip)
+
+    if ('.zip' + os.sep) in path:
+        (parent_zip_path, child_zip_path) = os.path.relpath(path).split(
+            '.zip' + os.sep, 1)
+        parent_zip_path += '.zip'
+
+        if not parent_zip:
+            # This is the top-level, so read from disk
+            parent_zip = zipfile.ZipFile(parent_zip_path)
+        else:
+            # We're already in a zip, so pull it out and recurse
+            parent_zip = extract_inner_zipfile(parent_zip, parent_zip_path)
+
+        return extract_nested_zipfile(child_zip_path, parent_zip)
+    else:
+        if parent_zip:
+            return extract_inner_zipfile(parent_zip, path)
+        else:
+            # If there is no nesting, it's easy!
+            return zipfile.ZipFile(path)
+
+
 def get_zoneinfo():
     """Cache the opened zipfile in the module."""
     global zoneinfo
     if zoneinfo is None:
-        zoneinfo = zipfile.ZipFile(zoneinfo_path)
+        zoneinfo = extract_nested_zipfile(zoneinfo_path)
 
     return zoneinfo
 
